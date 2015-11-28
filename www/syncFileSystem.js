c41e1e1ae229c755908853eebf0781ac21ba9159
komacke <davek@komacke.com>
2015-11-27 21:03:54 -0800

    arg - xhr, not exports.

--- www/syncFileSystem.js
+++ www/syncFileSystem.js
@@ -555,7 +555,7 @@ function deleteFile(fileName, callback) {
 // This function downloads the given Drive file.
 function downloadFile(file, callback) {
     // Send a request to retrieve the changes.
-    exports.get(file.downloadUrl).then(
+    xhr.get(file.downloadUrl).then(
         function(fileEntry) {
             return saveData(file.title, xhr.responseText);
         },
